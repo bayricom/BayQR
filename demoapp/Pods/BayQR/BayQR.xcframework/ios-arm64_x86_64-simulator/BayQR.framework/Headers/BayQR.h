27e9d99f617994df60f7d7b0efc69522a14a4eef
bayricom <github@bayri.com.tr>
2024-03-20 19:21:12 +0300

    xcframework

---
+++ demoapp/Pods/BayQR/BayQR.xcframework/ios-arm64_x86_64-simulator/BayQR.framework/Headers/BayQR.h
@@ -0,0 +1,18 @@
+//
+//  BayQR.h
+//  BayQR
+//
+//  Created by Bayri on 13.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BayQR.
+FOUNDATION_EXPORT double BayQRVersionNumber;
+
+//! Project version string for BayQR.
+FOUNDATION_EXPORT const unsigned char BayQRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BayQR/PublicHeader.h>
+
+
